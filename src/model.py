8e7897ae8beaee8e9c780c93b3ff098e679901ed
Thamirawaran Sathiyalogeswaran <107134124+Thamirawaran@users.noreply.github.com>
2024-10-15 14:09:59 +0530

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,12 +1,76 @@
-# src/model.py
+#import from zoo_1_gui, video_show;
+node Section{
+    has name:str,
+    position:tuple,
+    video_path:str = "",
+    image_path:str ="",
+    des:str ="";
+    
+    # can show with entry{
+    #     Video_show();
+    # }
+}
 
-from dataclasses import dataclass, field
-from typing import List, Optional
+edge Road{
+    has distance:int =0;
+}
 
-@dataclass
-class Section:
-    name: str
-    position: tuple  # (x, y) coordinates on the screen
-    image_path: str  # Path to the section image
-    enabled: bool = False  # Indicates if the section is clickable
-    connected_sections: List['Section'] = field(default_factory=list)  # Sections connected to this one
+obj Zoo{
+    can create_zoo(Entry:Any){
+        Entry = Section("Entry",(50,50));
+        Animal = Section("Animal",(50,50));
+        Bird = Section("Bird",(50,50));
+        Lion = Section("Lion",(50,50));
+        Elephant = Section("Elephant",(50,50));
+        Parrot = Section("Parrot",(50,50));
+        Humming = Section("Humming",(50,50));
+
+        Road_0 = Road(5);
+        Road_1 = Road(10);
+        Road_2 = Road(10);
+        Road_3 = Road(15);
+        Road_4 = Road(10);
+        Road_5 = Road(15);
+
+        root ++> Entry;
+        Entry +:Road_0:+> Animal;
+        Entry +:Road_1:+> Bird;
+        Animal +:Road_2:+> Lion;
+        Animal +:Road_3:+> Elephant;
+        Bird +:Road_4:+> Parrot;
+        Bird +:Road_5:+> Humming;
+    }
+}
+
+walker Visitor{
+    has name:str;
+    can Present with entry{
+        self.current = here;
+        print(self.current);
+        # visit [-->];
+        # Next = [-->];
+        # print(Next);
+    }
+
+    can Visit(){
+        #self.current = here;
+        while [self.current -->]{
+            for i in [self.current-->]{
+                print(i.name);
+                self.current = eval(i.name);
+            }
+            
+        }
+    }
+}
+
+
+with entry{
+    
+    zoo= Zoo().create_zoo(Entry);
+    # zoo.create_zoo();
+    Thami = Visitor("Thami");
+    root spawn Thami;
+    Thami.Visit();
+    
+}
